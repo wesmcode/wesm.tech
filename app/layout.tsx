3819e7cb6eef3d2753c68f6c95c50190fcdb21b0
wes <sync.wesm+projects@gmail.com>
2025-03-11 17:18:48 -0300

    Feat(ui): add favicon support using favicon.io

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,26 @@ export const metadata: Metadata = {
   generator: 'Next.js',
   keywords: ['Wesley Melo', 'Product Manager', 'wesm.tech'],
   authors: [{ name: 'Wesley Melo', url: 'https://wesm.tech' }],
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+    other: [
+      {
+        rel: 'android-chrome-192x192',
+        url: '/android-chrome-192x192.png',
+      },
+      {
+        rel: 'android-chrome-512x512',
+        url: '/android-chrome-512x512.png',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
@@ -16,6 +36,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body>{children}</body>
     </html>
   )
